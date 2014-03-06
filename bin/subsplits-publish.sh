fc7dcc275620e626d5f2cdb53eb6ed93fcd61fcd
Olivier Dolbeau <contact@odolbeau.fr>
2014-03-06 14:23:47 +0100

    Add some scripts to deal with subtree

---
+++ bin/subsplits-publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Using git-subsplit
+# https://github.com/dflydev/git-subsplit
+
+GIT_SUBSPLIT=$(pwd)/$(dirname $0)/git-subsplit.sh
+
+$GIT_SUBSPLIT init https://github.com/swarrot/swarrot
+
+$GIT_SUBSPLIT update
+
+$GIT_SUBSPLIT publish "
+    src/Swarrot/Processor/Ack:git@github.com:swarrot/ack-processor.git
+" --heads=master
