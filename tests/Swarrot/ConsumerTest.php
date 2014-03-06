0262983892f80e04456743f87735d8f76766ece2
Olivier Dolbeau <contact@odolbeau.fr>
2014-03-06 16:17:01 +0100

    Rewrite all tests with phpunit / prophecy

---
+++ tests/Swarrot/ConsumerTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Swarrot;
+
+use Prophecy\Argument;
+use Swarrot\Broker\MessageProviderInterface;
+use Swarrot\Processor\ProcessorInterface;
+use Swarrot\Processor\ConfigurableInterface;
+use Swarrot\Processor\InitializableInterface;
+use Swarrot\Processor\TerminableInterface;
+use Swarrot\Processor\SleepyInterface;
+use Swarrot\Broker\Message;
+
+class ConsumerTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->prophet = new \Prophecy\Prophet;
+    }
+
+    protected function tearDown()
+    {
+        $this->prophet->checkPredictions();
+    }
+
+    function test_it_is_initializable()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertInstanceOf('Swarrot\Consumer', $consumer);
+    }
+
+    function test_it_returns_null_if_no_error_occured()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+
+        $message = new Message(1, 'body');
+
+        $provider->get()->willReturn($message);
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::type('array')
+        )->willReturn(false);
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertNull($consumer->consume());
+    }
+
+    function test_it_call_processor_if_its_configurable()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ConfigurableInterface');
+
+        $message = new Message(1, 'body');
+
+        $provider->get()->willReturn($message);
+        $processor->setDefaultOptions(
+            Argument::type('Symfony\Component\OptionsResolver\OptionsResolverInterface')
+        )->willReturn(null);
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::type('array')
+        )->willReturn(false);
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertNull($consumer->consume());
+    }
+
+    function test_it_call_processor_if_its_initializable()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\InitializableInterface');
+
+        $message = new Message(1, 'body');
+
+        $provider->get()->willReturn($message);
+        $processor->initialize(Argument::type('array'))->willReturn(null);
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::type('array')
+        )->willReturn(false);
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertNull($consumer->consume());
+    }
+
+    function test_it_call_processor_if_its_terminable()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\TerminableInterface');
+
+        $message = new Message(1, 'body');
+
+        $provider->get()->willReturn($message);
+        $processor->terminate(Argument::type('array'))->willReturn(null);
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::type('array')
+        )->willReturn(false);
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertNull($consumer->consume());
+    }
+
+    function test_it_call_processor_if_its_Sleepy()
+    {
+        $provider  = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $processor = $this->prophet->prophesize('Swarrot\Processor\SleepyInterface');
+
+        $message = new Message(1, 'body');
+
+        $provider->get()->willReturn($message);
+        $processor->sleep(Argument::type('array'))->willReturn(null);
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::type('array')
+        )->willReturn(false);
+
+        $consumer = new Consumer($provider->reveal(), $processor->reveal());
+        $this->assertNull($consumer->consume());
+    }
+}
---
+++ tests/Swarrot/Processor/Stack/StackedProcessorTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Swarrot\Processor\Stack;
+
+use Swarrot\Processor\InitializableInterface;
+use Swarrot\Processor\TerminableInterface;
+use Swarrot\Processor\ProcessorInterface;
+use Swarrot\Processor\SleepyInterface;
+use Swarrot\Processor\Stack\StackedProcessor;
+use Swarrot\Broker\Message;
+use Prophecy\Argument;
+
+class StackedProcessorSpec extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->prophet = new \Prophecy\Prophet;
+    }
+
+    protected function tearDown()
+    {
+        $this->prophet->checkPredictions();
+    }
+
+    function test_it_is_initializable()
+    {
+        $p1  = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+        $p2  = $this->prophet->prophesize('Swarrot\Processor\InitializableInterface');
+        $p3  = $this->prophet->prophesize('Swarrot\Processor\TerminableInterface');
+        $p4  = $this->prophet->prophesize('Swarrot\Processor\SleepyInterface');
+
+        $stackedProcessor = new StackedProcessor(
+            $p1->reveal(), array(
+                $p2->reveal(),
+                $p3->reveal(),
+                $p4->reveal()
+            )
+        );
+
+        $this->assertInstanceOf('Swarrot\Processor\Stack\StackedProcessor', $stackedProcessor);
+    }
+
+    public function test_it_is_callable()
+    {
+        $p1  = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+        $p2  = $this->prophet->prophesize('Swarrot\Processor\InitializableInterface');
+        $p3  = $this->prophet->prophesize('Swarrot\Processor\TerminableInterface');
+        $p4  = $this->prophet->prophesize('Swarrot\Processor\SleepyInterface');
+
+        $p2->initialize(Argument::type('array'))->willReturn(null);
+        $p3->terminate(Argument::type('array'))->willReturn(null);
+        $p4->sleep(Argument::type('array'))->willReturn(null);
+
+        $p1->__invoke(Argument::type('Swarrot\Broker\Message'), Argument::type('array'))->willReturn(null);
+        $p2->__invoke(Argument::type('Swarrot\Broker\Message'), Argument::type('array'))->willReturn(null);
+        $p3->__invoke(Argument::type('Swarrot\Broker\Message'), Argument::type('array'))->willReturn(null);
+        $p4->__invoke(Argument::type('Swarrot\Broker\Message'), Argument::type('array'))->willReturn(null);
+
+        $stackedProcessor = new StackedProcessor(
+            $p1->reveal(), array(
+                $p2->reveal(),
+                $p3->reveal(),
+                $p4->reveal()
+            )
+        );
+
+        $this->assertNull($stackedProcessor->initialize(array()));
+        $this->assertNull($stackedProcessor->terminate(array()));
+        $this->assertNull($stackedProcessor->sleep(array()));
+
+        $this->assertNull($stackedProcessor->__invoke(
+            new Message(1, 'body'),
+            array()
+        ));
+    }
+}
