93dd1d71b82054fbdee9d9513c8efb80b04baffe
remy lemeunier <remy.lemeunier@blablacar.com>
2014-03-06 11:49:09 +0100

    First implementation of MaxMessages.

---
+++ README.md
@@ -0,0 +1,38 @@
+# MaxMessagesProcessor
+
+[![Build Status](https://travis-ci.org/swarrot/max-messages-processor.png)](https://travis-ci.org/swarrot/max-messages-processor)
+
+MaxMessagesProcessor is a [swarrot](https://github.com/swarrot/swarrot) processor.
+The goal of this processor is to stop when X messages have been processed.
+
+## Installation
+
+The recommended way to install MaxMessagesProcessor is through
+[Composer](http://getcomposer.org/). Require the `swarrot/max-messages-processor` package
+into your `composer.json` file:
+
+```json
+{
+    "require": {
+        "swarrot/max-messages-processor": "@stable"
+    }
+}
+```
+
+**Protip:** you should browse the
+[`swarrot/max-messages-processor`](https://packagist.org/packages/swarrot/max-messages-processor)
+page to choose a stable version to use, avoid the `@stable` meta constraint.
+
+## Usage
+
+See [swarrot documentation](https://github.com/swarrot/swarrot).
+
+## Configuration
+
+|Key             |Default|Description                                               |
+|:--------------:|:-----:|----------------------------------------------------------|
+|max_messages    |100    |Set the maximum number of messages processed by the worker|
+
+## License
+
+MaxMessagesProcessor is released under the MIT License. See the bundled LICENSE file for details.
---
+++ tests/Swarrot/Processor/MaxMessagesProcessorTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Swarrot\Processor;
+
+use Swarrot\Processor\MaxMessagesProcessor;
+use Prophecy\Argument;
+use Swarrot\Broker\Message;
+
+class MaxMessagesProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->prophet = new \Prophecy\Prophet;
+    }
+
+    protected function tearDown()
+    {
+        $this->prophet->checkPredictions();
+    }
+
+    public function test_it_is_initializable_without_a_logger()
+    {
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+        $messageProvider    = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+
+        $processor = new MaxMessagesProcessor($processor->reveal(), $messageProvider->reveal());
+        $this->assertInstanceOf('Swarrot\Processor\MaxMessagesProcessor', $processor);
+    }
+
+    public function test_it_is_initializable_with_a_logger()
+    {
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+        $messageProvider    = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $logger             = $this->prophet->prophesize('Psr\Log\LoggerInterface');
+
+        $processor = new MaxMessagesProcessor($processor->reveal(), $messageProvider->reveal(), $logger->reveal());
+        $this->assertInstanceOf('Swarrot\Processor\MaxMessagesProcessor', $processor);
+    }
+
+    public function test_count_default_messages_processed()
+    {
+        $maxMessages = 2;
+        $processor = $this->prophet->prophesize('Swarrot\Processor\ProcessorInterface');
+        $processor->__invoke(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::exact(array(
+                'max_messages' => $maxMessages,
+            ))
+        )
+        ->shouldBeCalledTimes(2);
+
+        $messageProvider    = $this->prophet->prophesize('Swarrot\Broker\MessageProviderInterface');
+        $logger    = $this->prophet->prophesize('Psr\Log\LoggerInterface');
+        $logger->debug(
+            Argument::exact(sprintf('Max messages have been reached (%d)', $maxMessages))
+        )
+        ->shouldBeCalledTimes(1);
+
+        $message = new Message(1, 'body');
+        $processor = new MaxMessagesProcessor(
+            $processor->reveal(),
+            $messageProvider->reveal(),
+            $logger->reveal()
+        );
+
+        // Process
+        $this->assertNull($processor->__invoke($message, array('max_messages' => $maxMessages)));
+        // Process
+        $this->assertNull($processor->__invoke($message, array('max_messages' => $maxMessages)));
+
+        // Too much messages processed, return false
+        $this->assertFalse($processor->__invoke($message, array('max_messages' => $maxMessages)));
+    }
+}
