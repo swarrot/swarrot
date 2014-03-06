596eef330403641deac5b24a2cd5735e62add424
Olivier Dolbeau <contact@odolbeau.fr>
2014-03-06 15:32:37 +0100

    Add correct config for all subtrees

--- bin/subsplits-publish.sh
+++ bin/subsplits-publish.sh
@@ -11,5 +11,9 @@ $GIT_SUBSPLIT update
 
 $GIT_SUBSPLIT publish "
     src/Swarrot/Processor/Ack:git@github.com:swarrot/ack-processor.git
+    src/Swarrot/Processor/ExceptionCatcher:git@github.com:swarrot/exception-catcher-processor.git
+    src/Swarrot/Processor/InstantRetry:git@github.com:swarrot/instant-retry-processor.git
     src/Swarrot/Processor/MaxExecutionTime:git@github.com:swarrot/max-execution-time-processor.git
+    src/Swarrot/Processor/MaxMessages:git@github.com:swarrot/max-messages-processor.git
+    src/Swarrot/Processor/SignalHandler:git@github.com:swarrot/signal-handler-processor.git
 " --heads=master
