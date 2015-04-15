43cc01c1fc664a47a9e382cb415dfa8d2ce89707
Christophe Coevoet <stof@notk.org>
2015-04-15 12:13:29 +0200

    Implement the MemoryLimitProcessor

---
+++ tests/Swarrot/Processor/MemoryLimit/MemoryLimitProcessorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Swarrot\Processor\MemoryLimit;
+
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTestCase;
+use Swarrot\Broker\Message;
+
+class MemoryLimitProcessorTest extends ProphecyTestCase
+{
+    public function test_it_is_initializable_without_a_logger()
+    {
+        $processor = $this->prophesize('Swarrot\Processor\ProcessorInterface');
+
+        $processor = new MemoryLimitProcessor($processor->reveal());
+        $this->assertInstanceOf('Swarrot\Processor\MemoryLimit\MemoryLimitProcessor', $processor);
+    }
+
+    public function test_it_is_initializable_with_a_logger()
+    {
+        $processor = $this->prophesize('Swarrot\Processor\ProcessorInterface');
+        $logger    = $this->prophesize('Psr\Log\LoggerInterface');
+
+        $processor = new MemoryLimitProcessor($processor->reveal(), $logger->reveal());
+        $this->assertInstanceOf('Swarrot\Processor\MemoryLimit\MemoryLimitProcessor', $processor);
+    }
+
+    public function test_delegate_processing()
+    {
+        $processor = $this->prophesize('Swarrot\Processor\ProcessorInterface');
+        $processor->process(
+            Argument::type('Swarrot\Broker\Message'),
+            Argument::exact(array(
+                'memory_limit' => null,
+            ))
+        )
+        ->shouldBeCalledTimes(1);
+
+        $logger = $this->prophesize('Psr\Log\LoggerInterface');
+
+        $message = new Message('body', array(), 1);
+        $processor = new MemoryLimitProcessor(
+            $processor->reveal(),
+            $logger->reveal()
+        );
+
+        // Process
+        $this->assertNull($processor->process($message, array('memory_limit' => null)));
+    }
+}
