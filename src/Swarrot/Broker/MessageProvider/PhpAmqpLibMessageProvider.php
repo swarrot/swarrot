2d8b43bbbeccc5d870931159c39b9600b2831367
Jean-Guilhem Rouel <jean-gui@w3.org>
2015-06-17 15:10:05 +0200

    Fix #101: Don't set default values for non-existing properties in PhpAmqpLibLMessageProvider

--- src/Swarrot/Broker/MessageProvider/PhpAmqpLibMessageProvider.php
+++ src/Swarrot/Broker/MessageProvider/PhpAmqpLibMessageProvider.php
@@ -38,32 +38,18 @@ class PhpAmqpLibMessageProvider implements MessageProviderInterface
             return;
         }
 
-        // Explanation on these properties can be found at:
-        // https://github.com/videlalvaro/php-amqplib/blob/091/doc/AMQPMessage.md
-        $properties = array(
-            // Here are all the supported message properties by the php-amqplib library
-            'content_type'     => $envelope->has('content_type')        ? $envelope->get('content_type')        : '',
-            'delivery_mode'    => $envelope->has('delivery_mode')       ? $envelope->get('delivery_mode')       : 0,
-            'content_encoding' => $envelope->has('content_encoding')    ? $envelope->get('content_encoding')    : '',
-            'type'             => $envelope->has('type')                ? $envelope->get('type')                : '',
-            'timestamp'        => $envelope->has('timestamp')           ? $envelope->get('timestamp')           : 0,
-            'priority'         => $envelope->has('priority')            ? $envelope->get('priority')            : 0,
-            'expiration'       => $envelope->has('expiration')          ? $envelope->get('expiration')          : '',
-            'app_id'           => $envelope->has('app_id')              ? $envelope->get('app_id')              : '',
-            'message_id'       => $envelope->has('message_id')          ? $envelope->get('message_id')          : '',
-            'reply_to'         => $envelope->has('reply_to')            ? $envelope->get('reply_to')            : '',
-            'correlation_id'   => $envelope->has('correlation_id')      ? $envelope->get('correlation_id')      : '',
-            'user_id'          => $envelope->has('user_id')             ? $envelope->get('user_id')             : 0,
-            'cluster_id'       => $envelope->has('cluster_id')          ? $envelope->get('cluster_id')          : 0,
+        $properties = [];
+        $propertyKeys = [
+            'content_type', 'delivery_mode', 'content_encoding', 'type', 'timestamp', 'priority', 'expiration',
+            'app_id', 'message_id', 'reply_to', 'correlation_id', 'user_id', 'cluster_id', 'channel', 'consumer_tag',
+            'delivery_tag', 'redelivered', 'exchange', 'routing_key'
+        ];
 
-            // Following parameters are delivery information added by the php-amqplib library
-            'channel'       => isset($envelope->delivery_info['channel'])      ? $envelope->delivery_info['channel']      : '',
-            'consumer_tag'  => isset($envelope->delivery_info['consumer_tag']) ? $envelope->delivery_info['consumer_tag'] : '',
-            'delivery_tag'  => isset($envelope->delivery_info['delivery_tag']) ? $envelope->delivery_info['delivery_tag'] : '',
-            'is_redelivery' => isset($envelope->delivery_info['redelivered'])  ? $envelope->delivery_info['redelivered']  : false,
-            'exchange_name' => isset($envelope->delivery_info['exchange'])     ? $envelope->delivery_info['exchange']     : '',
-            'routing_key'   => isset($envelope->delivery_info['routing_key'])  ? $envelope->delivery_info['routing_key']  : '',
-        );
+        foreach($propertyKeys as $key) {
+            if ($envelope->has($key)) {
+                $properties[$key] = $envelope->get($key);
+            }
+        }
 
         $properties['headers'] = [];
         if ($envelope->has('application_headers')) {
