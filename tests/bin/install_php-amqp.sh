ca9950f189e1bb3fe404759c0929751d33ce2a92
Olivier Dolbeau <git@a.bbnt.me>
2017-11-01 14:06:06 +0100

    Update lib_rabbitmq installation script

---
+++ tests/bin/install_php-amqp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -x
+
+echo "# Installing php-amqp ${PHPAMQP_VERSION}"
+if [ ! -d "$HOME/php-amqp" ]; then
+  cd $HOME
+  git clone git://github.com/pdezwart/php-amqp.git
+  cd $HOME/php-amqp
+else
+  echo 'Using cached directory.';
+  cd $HOME/php-amqp
+  git fetch
+fi
+
+git checkout ${PHPAMQP_VERSION}
+
+phpize --clean && phpize && ./configure && sudo make install
+echo "extension = amqp.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
